1e6d3dd613987c8dc0c52d6ca948e6673ed54ece
Daedelus1 <109355795+Daedelus1@users.noreply.github.com>
2024-09-01 09:06:22 -0500

    Added Toggling Button Template

--- src/user-input-templates.cpp
+++ src/user-input-templates.cpp
@@ -6,7 +6,7 @@ using namespace std::chrono;
 /*
 These are a collection of presets you can use to set up
 the various user input devices.
-A note: It is *critical* that you set the value for the BUTTON_OBJECT.
+A note: It is *critical* that you set the value for the buttonObject.
 The code will compile but it will have undefined behavoir (meaning it will act randomly),
 which is a pretty awful thing to debug
 
@@ -21,7 +21,7 @@ so it is okay that the compiler gives a warning about it.
 // the most basic button template
 namespace emptyButtonTemplate
 {
-    static vex::controller::button BUTTON_OBJECT{};
+    static vex::controller::button buttonObject{/*REPLACE ME!!!*/};
     void onPress()
     {
     }
@@ -33,14 +33,14 @@ namespace emptyButtonTemplate
     }
     void initialize()
     {
-        BUTTON_OBJECT.pressed(onPress);
-        BUTTON_OBJECT.released(onRelease);
+        buttonObject.pressed(onPress);
+        buttonObject.released(onRelease);
     }
 }
 // continually acts while the respective button is pressed
 namespace whilePressedButtonTemplate
 {
-    static vex::controller::button BUTTON_OBJECT{};
+    static vex::controller::button buttonObject{/*REPLACE ME!!!*/};
     static bool buttonIsPressed{false};
     static void onPress()
     {
@@ -58,20 +58,49 @@ namespace whilePressedButtonTemplate
         {
             return;
         }
-        // toggled action here
+        // repeated action here
     }
     static void initialize()
     {
-        BUTTON_OBJECT.pressed(onPress);
-        BUTTON_OBJECT.released(onRelease);
+        buttonObject.pressed(onPress);
+        buttonObject.released(onRelease);
     }
 
 }
+// alternates between doing action A and B, used for such things as pneumatic controls
+namespace ToggleActionButton
+{
+    static vex::controller::button buttonObject{/*REPLACE ME!!!*/};
+    static bool latch{false};
+    void onPress()
+    {
+        if (latch)
+        {
+            // do action A
+        }
+        else
+        {
+            // do action B
+        }
+        latch = !latch;
+    }
+    void onRelease()
+    {
+    }
+    void onPing()
+    {
+    }
+    void initialize()
+    {
+        buttonObject.pressed(onPress);
+        buttonObject.released(onRelease);
+    }
+}
 // acts once after a constant delay
-// please do not use vex::waitUntil, because we only have one thread to work with
+// please do not use vex::waitUntil, because the other buttonPings have to wait until each previous onPing() is done
 namespace actAfterDelayButtonTemplate
 {
-    static vex::controller::button BUTTON_OBJECT{};
+    static vex::controller::button buttonObject{/*REPLACE ME!!!*/};
     static bool latch{false};
     static milliseconds timeOfLastPressMS{};
     const static milliseconds actionDelayMS{100};
@@ -104,14 +133,14 @@ namespace actAfterDelayButtonTemplate
     }
     void initialize()
     {
-        BUTTON_OBJECT.pressed(onPress);
-        BUTTON_OBJECT.released(onRelease);
+        buttonObject.pressed(onPress);
+        buttonObject.released(onRelease);
     }
 }
 namespace emptyJoystickTemplate
 {
-    static vex::controller::axis xAxis{};
-    static vex::controller::axis yAxis{};
+    static vex::controller::axis xAxis{/*REPLACE ME!!!*/};
+    static vex::controller::axis yAxis{/*REPLACE ME!!!*/};
     void onPing()
     {
     }
